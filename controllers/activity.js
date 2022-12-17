e6ebd06f00315ee113ae8622e19c2967ee1d77e5
Patipan.m <114389220+flooksan@users.noreply.github.com>
2022-12-17 22:21:33 +0700

    Update: Route and controllers of totalstatus

--- controllers/activity.js
+++ controllers/activity.js
@@ -64,31 +64,32 @@ exports.editActivity = (req,res)=> {
     })
 }
 
-// 0=Pending,1 =  Completed , 9 = Incomplete
+
 exports.changeStatus = (req,res)=> {
     const {id} = req.params
     // console.log(id)
     const { status } = req.body
     // console.log(req.body)
-    activitys.findOneAndUpdate({_id: new ObjectId(`${id}`)},{status},{new:true})
+    activitys.findOneAndUpdate({_id: id},{status},{new:true})
     .exec((err,activity) => {
         if(err) { res.status(400).json({error:`Error code : ${err}`}) }
         res.send(activity)
     })
 }
 
-exports.totalStatus = (req,res)=> {
-    const user = 'sarapao.peetgungunMark@gmail.com' // wait to change
-    const {id} = req.params
-    // console.log(id)
-    const { status } = req.body
-    // console.log(req.body)
-    db.activitys.aggregate([{$match:{'username': user } },{$group:{_id: "$status",totalscore:{ $sum: 1}}}])
+exports.getTotalStatus = (req,res)=> {
+
+    const user = req.query.user // wait to change
+
+    activitys.aggregate([{$match:{'username': user } },{$group:{_id: "$status",totalscore:{ $sum: 1}}}])
     .exec((err,activity) => {
         if(err) { res.status(400).json({error:`Error code : ${err}`}) }
+        console.log(activity)
         res.send(activity)
     })
 }
+
+// 0=Pending,1 =  Completed , 9 = Incomplete
 
 
 exports.removeOneCard = (req,res) => {
--- routes/activity.js
+++ routes/activity.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const router = express.Router()
 // const activityModel = require('../models/activityCard')
-const {getData,getChart,createActivity,getOneCard,removeOneCard,editActivity,changeStatus} = require('../controllers/activity')
+const {getData,getChart,createActivity,getOneCard,removeOneCard,editActivity,changeStatus,getTotalStatus} = require('../controllers/activity')
 
 
 router.get('/card-activity',getData)
 router.get('/chart-activity',getChart)
+router.get('/total-status',getTotalStatus)
 router.post('/add-activity',createActivity)
 router.get('/card-activity/:id',getOneCard)
 router.delete('/card-activity/:id',removeOneCard)
 router.put('/edit-activity/:id',editActivity)
 router.put('/change-status/:id',changeStatus)
+
     
 
 
